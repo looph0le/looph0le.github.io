fbd7bd795cec35d1003f12a168cde23ed1dbb1be
looph0le <mitanshpanchal@gmail.com>
2022-06-04 19:20:27 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'red': '#F50A5E',
+      'purple': '#3f3cbb',
+      'midnight': '#02030C',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+}
