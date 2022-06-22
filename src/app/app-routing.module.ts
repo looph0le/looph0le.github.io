4a35fc26f956f129b34a12acb63fc023f4cf50ce
looph0le <mitanshpanchal@gmail.com>
2022-06-22 13:11:50 +0530

    alot new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
+import { ServicesComponent } from './services/services.component'
+import { SolutionsComponent } from './solutions/solutions.component'
+import { ContactComponent } from './contact/contact.component'
+
+const routes: Routes = [
+  { path: '' , component: HomeComponent },
+  { path: 'portfolio' , component: PortfolioComponent },
+  { path: 'services' , component: ServicesComponent },
+  { path: 'solutions' , component: SolutionsComponent },
+  { path: 'contact' , component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { UpdatesComponent } from './updates/updates.component';
+import { ServicesComponent } from './services/services.component';
+import { SolutionsComponent } from './solutions/solutions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    UpdatesComponent,
+    ServicesComponent,
+    SolutionsComponent
   ],
   imports: [
     BrowserModule,
